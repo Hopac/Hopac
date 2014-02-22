d32190842ebba1566c921644481fa068a39f3148
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-02-22 23:03:26 +0200

    Rewrote benchmark to be a more accurate translation.  Several other variations are possible.

--- Benchmarks/CounterActor/CounterActor.fs
+++ Benchmarks/CounterActor/CounterActor.fs
@@ -2,74 +2,64 @@
 
 module CounterActor
 
+// Inspired by: http://zbray.com/2012/12/09/building-an-actor-in-f-with-higher-throughput-than-akka-and-erlang-actors/
+
+// Note that within Hopac there are many ways to implement something similar
+// to the kind of actor described in that blog post.
+
+open System
+open System.Diagnostics
 open Hopac
 open Hopac.Extensions
 open Hopac.Job.Infixes
-open System
-open System.Diagnostics
 
-module Native =
-  open System.Threading.Tasks
+module CounterActorChMsg =
+  type Msg =
+   | Add of int64
+   | GetAndReset of IVar<int64>
 
-  type CounterActor () =
-    let lockObj = obj ()
-    let mutable count = 0L
-    member self.Add n : unit =
-      lock lockObj <| fun () ->
-      count <- count + n
-    member self.GetAndReset () : int64 =
-      lock lockObj <| fun () ->
-      let result = count
-      count <- 0L
-      result
+  type CounterActor =
+   | CA of Ch<Msg>
 
-  let run title n =
-    printf "%s: " title
-    let actor = new CounterActor ()
-    let timer = Stopwatch.StartNew ()
-    let r =
-      Parallel.For (0, n, fun i -> actor.Add 100L) |> ignore
-      actor.GetAndReset ()
-    let d = timer.Elapsed
-    printf "%f ops/s - %d ops - result is %A\n" (float n / d.TotalSeconds) n r
+  let create : Job<CounterActor> = job {
+    let inCh = Ch.Now.create ()
+    let state = ref 0L
+    do! Job.start
+         (Job.forever
+           (Ch.take inCh >>= function
+             | Add n ->
+               state := !state + n
+               Job.unit
+             | GetAndReset replyVar ->
+               let was = !state
+               state := 0L
+               IVar.fill replyVar was))
+    return (CA inCh)
+  }
 
-module Hopac =
-  type CounterActor () =
-    let mutable count = 0L
-    let lock = Lock.Now.create ()
-    member self.Add n : Job<unit> =
-      Lock.duringFun lock <| fun () -> count <- count + n
-    member self.GetAndReset : Job<int64> =
-      Lock.duringFun lock <| fun () ->
-      let result = count
-      count <- 0L
-      result
+  let add (CA inCh) (n: int64) : Job<unit> = Ch.give inCh (Add n)
+  let getAndReset (CA inCh) : Job<int64> = job {
+    let replyVar = IVar.Now.create ()
+    do! Ch.give inCh (GetAndReset replyVar)
+    return! IVar.read replyVar
+  }
 
-  let run title n m =
-    printf "%s: " title
-    let timer = Stopwatch.StartNew ()
-    let actor = CounterActor ()
-    let r =
-      Job.Now.run <| job {
-        let! promises =
-          seq {1 .. m}
-          |> Seq.Parallel.mapJ (fun _ ->
-             Promise.start <| job {
-               do! Job.forN n (actor.Add 100L)
-               return! actor.GetAndReset
-             })
-        return! promises |> Seq.Parallel.mapJ Promise.read
-      }
-    let d = timer.Elapsed
-    printf "%f ops/s - %d*%d ops - result is %A\n" (float (n*m) / d.TotalSeconds) n m r
-
-do Native.run "native" 30000
-   GC.Collect ()
-   System.Threading.Thread.Sleep (TimeSpan.FromSeconds 5.0)
-   Native.run "native" 30000000
-   GC.Collect ()
-   System.Threading.Thread.Sleep (TimeSpan.FromSeconds 5.0)
-   Hopac.run "warmup" 10000 Environment.ProcessorCount
-   GC.Collect ()
-   System.Threading.Thread.Sleep (TimeSpan.FromSeconds 5.0)
-   Hopac.run "benchm" 30000000 Environment.ProcessorCount
+let run numPerThread =
+  let timer = Stopwatch.StartNew ()
+  let r = Job.Now.run <| job {
+    let! actor = CounterActorChMsg.create
+    do! seq {1 .. Environment.ProcessorCount}
+        |> Seq.Parallel.iterJ
+            (fun _ -> Job.forN numPerThread (CounterActorChMsg.add actor 100L))
+    return! CounterActorChMsg.getAndReset actor
+  }
+  let d = timer.Elapsed
+  let m = sprintf "CounterActorChMsg: %d * %d msgs => %f msgs/s\n"
+           Environment.ProcessorCount numPerThread
+           (float (Environment.ProcessorCount * numPerThread) / d.TotalSeconds)
+  printf "%s" m
+ 
+do run 3000 ; GC.Collect () ; Threading.Thread.Sleep 500
+   run 30000 ; GC.Collect () ; Threading.Thread.Sleep 500
+   run 3000000 ; GC.Collect () ; Threading.Thread.Sleep 500
+   run 30000000
