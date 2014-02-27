60c43fb3b16dd6afa594c6d94d9c665ccbc43aaa
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-02-27 14:23:27 +0200

    Added Hopac.fsx for interacting with the library.

---
+++ Hopac.fsx
@@ -0,0 +1,11 @@
+// Tip: Add --lib:<hopac-root-directory> to your F# interactive options
+
+#r "Libs\\Hopac.Extra\\bin\\Release\\Hopac.Core.dll" ;;
+#r "Libs\\Hopac.Extra\\bin\\Release\\Hopac.dll" ;;
+#r "Libs\\Hopac.Extra\\bin\\Release\\Hopac.Extra.dll" ;;
+
+open System ;;
+open Hopac ;;
+open Hopac.Job.Infixes ;;
+open Hopac.Alt.Infixes ;;
+open Hopac.Extensions ;;
