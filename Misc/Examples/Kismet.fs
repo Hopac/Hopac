9b5236d843e17d50dd6707d7751064793d1eb98c
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-03-13 11:39:32 +0200

    Refined Kismet snippets.

--- Misc/Examples/Kismet.fs
+++ Misc/Examples/Kismet.fs
@@ -1,27 +1,45 @@
 ﻿module Kismet
 
+open System
 open Hopac
+open Hopac.Alt
 open Hopac.Alt.Infixes
+open Hopac.Job
 open Hopac.Job.Infixes
 
 // Inspired by http://upload.wikimedia.org/wikipedia/en/e/e6/Kismet_Roboblitz.PNG
 
-let CompareBool comparand inCh onTrueCh onFalseCh =
-  Job.forever
-   (Ch.take inCh >>= fun x ->
-    Ch.give (if !comparand then onTrueCh else onFalseCh) x)
+let CompareBool comparand input onTrue onFalse =
+  pick input >>= fun x ->
+  if !comparand then onTrue x else onFalse x
 
-let Delay duration startCh stopCh finishedCh abortedCh =
-  let rec initial () = Ch.take startCh >>= started
-  and started x =
-    Alt.pick (Ch.Alt.take stopCh      >=> Ch.give abortedCh
-          <|> Alt.timeOut (!duration) >=> fun () -> Ch.give finishedCh x) >>= initial
-  initial ()
+let Delay duration start stop finished aborted =
+  pick start >>= fun x ->
+  select [stop                >=> fun y -> aborted y
+          timeOut (!duration) >=> fun () -> finished x]
 
-let Sink inCh = Job.forever (Ch.take inCh)
+let Set value target input output =
+  pick input >>= fun x ->
+  target := value
+  output x
 
-let Set value target inCh outCh =
-  Job.forever
-   (Ch.take inCh >>= fun x ->
-    target := value
-    Ch.give outCh x)
+let setup () = job {
+  let ch_1 = Ch.Now.create ()
+  let ch_2 = Ch.Now.create ()
+  let ch_3 = Ch.Now.create ()
+  // ...
+  let bMoved = ref false
+  // ...
+  do! CompareBool bMoved
+                  (Ch.Alt.take ch_1)
+                  (Ch.give ch_2)
+                  (fun _ -> Job.unit)
+      |> Job.forever |> Job.server
+  do! Delay (ref (TimeSpan.FromSeconds 3.14))
+            (Ch.Alt.take ch_2)
+            (Alt.never ())
+            (Ch.give ch_3)
+            (fun _ -> Job.unit)
+      |> Job.forever |> Job.server
+  // ...
+}
