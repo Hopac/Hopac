b1a048505e5da37bbfc00b973487d108540f6df3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-03-24 22:55:43 +0200

    Added simple TicketLock while doing another round of experimenting with the low level locking primitives.

--- Libs/Hopac.Core/Util/SpinlockTTAS.cs
+++ Libs/Hopac.Core/Util/SpinlockTTAS.cs
@@ -16,8 +16,6 @@ namespace Hopac.Core {
     internal const int Open   =  0;
     internal const int Locked = -1;
 
-    /////////////////////////////////////////////////////////////////////
-
     [MethodImpl(AggressiveInlining.Flag)]
     internal void Enter() {
     Spin:
@@ -33,74 +31,6 @@ namespace Hopac.Core {
     internal void Exit() {
       Debug.Assert(state == Locked); // Must be locked!
       state = Open;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal int GetState() {
-      return state;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal int TryEnter() {
-      return Interlocked.Exchange(ref state, Locked);
-    }
-
-    /////////////////////////////////////////////////////////////////////
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static void Enter(ref SpinlockTTAS self) {
-    Spin:
-      // First spin using shared reads.  This improves performance a bit.
-      var st = self.state;
-      if (st < Open)
-        goto Spin;
-      if (Interlocked.Exchange(ref self.state, ~st) < Open)
-        goto Spin;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static void Exit(ref SpinlockTTAS self) {
-      Debug.Assert(self.state == Locked); // Must be locked!
-      self.state = Open;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static int GetState(ref SpinlockTTAS self) {
-      return self.state;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static int TryEnter(ref SpinlockTTAS self) {
-      return Interlocked.Exchange(ref self.state, Locked);
-    }
-
-    /////////////////////////////////////////////////////////////////////
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static void Enter(ref int state) {
-    Spin:
-      // First spin using shared reads.  This improves performance a bit.
-      var st = state;
-      if (st < Open)
-        goto Spin;
-      if (Interlocked.Exchange(ref state, ~st) < Open)
-        goto Spin;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static void Exit(ref int state) {
-      Debug.Assert(state == Locked); // Must be locked!
-      state = Open;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static int GetState(ref int state) {
-      return state;
-    }
-
-    [MethodImpl(AggressiveInlining.Flag)]
-    internal static int TryEnter(ref int state) {
-      return Interlocked.Exchange(ref state, Locked);
     }
   }
 }
---
+++ Libs/Hopac.Core/Util/TicketLock.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (C) by Housemarque, Inc.
+
+namespace Hopac.Core {
+  using System.Diagnostics;
+  using System.Runtime.CompilerServices;
+  using System.Threading;
+
+  internal struct TicketLock {
+    internal volatile int Next;
+    internal volatile int Turn;
+
+    [MethodImpl(AggressiveInlining.Flag)]
+    internal void Enter() {
+      var ticket = Interlocked.Increment(ref Next) - 1;
+      while (Turn != ticket) ;
+    }
+
+    [MethodImpl(AggressiveInlining.Flag)]
+    internal void Exit() {
+      Turn += 1;
+    }
+  }
+}
