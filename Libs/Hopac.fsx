9a109666699890b93b2ed75688819c445e31a458
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-03-27 15:45:13 +0200

    Removed duplicate file.

--- Libs/Hopac.fsx
+++
@@ -1,9 +0,0 @@
-
-#r "Libs\\Hopac.Core\\bin\\Release\\Hopac.Core.dll"
-#r "Libs\\Hopac\\bin\\Release\\Hopac.dll"
-#r "Libs\\Hopac.Extra\\bin\\Release\\Hopac.Extra.dll"
-
-open Hopac
-open Hopac.Job.Infixes
-open Hopac.Alt.Infixes
-open Hopac.Extensions
