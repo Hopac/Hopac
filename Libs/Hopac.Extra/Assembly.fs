fa2db5dd28d324ef4bfa88d13b97dded2de17633
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-08-21 02:17:02 +0300

    Version and 4.5.1 -> 4.5.

--- Libs/Hopac.Extra/Assembly.fs
+++ Libs/Hopac.Extra/Assembly.fs
@@ -7,7 +7,7 @@ open System.Runtime.InteropServices
 [<AutoOpen>]
 module internal AssemblyInfo =
   [<Literal>]
-  let Version = "0.0.0.5"
+  let Version = "0.0.0.6"
 
 [<assembly: AssemblyTitle("Hopac.Extra")>]
 [<assembly: AssemblyDescription("Provides additional utilities for programming with Hopac.")>]
