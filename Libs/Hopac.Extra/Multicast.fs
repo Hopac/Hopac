e595eaa3712458838c8eba207de9d2c964ac247f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-11-19 14:22:10 +0200

    A commented out implementation of Multicast without the central server for reference.  Will need to properly test and benchmark before possibly switching to it.

--- Libs/Hopac.Extra/Multicast.fs
+++ Libs/Hopac.Extra/Multicast.fs
@@ -5,7 +5,9 @@ namespace Hopac.Extra
 open Hopac
 open Hopac.Infixes
 open Hopac.Job.Infixes
+open Hopac.Alt.Infixes
 
+//(*
 type MPort<'a> =
  | MPort of Ch<'a>
 
@@ -53,3 +55,37 @@ module Multicast =
   module Alt =
     let recv (MPort port) =
       asAlt port
+//*)
+(*
+type Stream<'x> = {
+  Value: 'x
+  Next: IVar<Stream<'x>>
+}
+
+type MChan<'x> = MChan of MVar<IVar<Stream<'x>>>
+type MPort<'x> = MPort of Ch<'x>
+
+module Multicast =
+  module Now =
+    let create () = MChan (mvarFull (ivar ()))
+
+  let create () = Job.thunk Now.create
+
+  let port (MChan xMc) =
+    MVar.read xMc >>= fun v ->
+    let outCh = ch ()
+    let tee v = v >>= fun r -> outCh <-- r.Value >>% r.Next
+    Job.iterateServer v tee >>%
+    MPort outCh
+
+  let multicast (MChan xMc) x =
+    xMc >>= fun v ->
+    let nV = ivar ()
+    (xMc <<-= nV) >>.
+    (v <-= {Value = x; Next = nV})
+
+  let recv (MPort xMp) = asJob xMp
+
+  module Alt =
+    let recv (MPort xMp) = asAlt xMp
+*)
