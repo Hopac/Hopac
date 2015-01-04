03ceacafef4bbb1a2ae4e61e923653b0a2576c57
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-01-05 00:28:52 +0200

    Reference platform dll and open Timer for convenience.

--- Hopac.fsx
+++ Hopac.fsx
@@ -4,11 +4,13 @@
 #r "Libs/Hopac/bin/Release/Hopac.dll" ;;
 #r "Libs/Hopac.Extra/bin/Release/Hopac.Extra.dll" ;;
 #r "Libs/Hopac.Experimental/bin/Release/Hopac.Experimental.dll" ;;
+#r "Libs/Hopac.Platform.Net/bin/Release/Hopac.Platform.dll" ;;
 
 open System ;;
 open Hopac.Experimental ;;
 open Hopac.Extensions ;;
 open Hopac.Extra ;;
+open Hopac.Timer.Global ;;
 open Hopac.Alt.Infixes ;;
 open Hopac.Job.Infixes ;;
 open Hopac.Infixes ;;
