4f1c2b99411d48698ff85309a23a28b90a4db42e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-03-01 16:21:25 +0200

    Add a test of the job computation builder.

---
+++ Tests/AdHocTests/BuilderTests.fs
@@ -0,0 +1,33 @@
+﻿// Copyright (C) by Vesa Karvonen
+
+module BuilderTests
+
+open Hopac
+open System
+open System.Threading.Tasks
+
+// This is a compile only test.
+let testSyntax (xT: Task<IDisposable>)
+               (t: Task)
+               (xA: Async<int>)
+               (xs: seq<float>)
+               (uJ: Job<unit>) = job {
+  let! _ = xT
+  let! _ = xA
+  do! t
+  do! uJ
+  if true then
+    return ()
+  use! x = xT
+  for _ in xs do
+    ()
+  try return! uJ
+  with _ -> ()
+  try return! t
+  finally ()
+  let n = ref 1
+  while !n > 0 do
+    n := !n-1
+    return! t
+  return x
+}
