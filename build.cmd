763439c2490bab835bbfdcc61a67f6a7aeac9366
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-22 11:08:43 +0300

    Try to run tests in AppVeyor

--- build.cmd
+++ build.cmd
@@ -5,3 +5,7 @@ if not exist packages\FAKE\tools\Fake.exe (
   paket.exe install
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
+
+REM Test
+msbuild /verbosity:quiet Hopac.sln
+Tests\AdHocTests\bin\Debug\AdHocTests.exe
