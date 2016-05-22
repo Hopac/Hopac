c7775f2feddaee6f46495db00d8585d516bb7213
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-22 21:12:11 +0300

    Using task builder.

--- Tests/AdHocTests/TaskTests.fs
+++ Tests/AdHocTests/TaskTests.fs
@@ -8,40 +8,45 @@ open Hopac.Extensions
 open System
 open System.Threading
 open System.Threading.Tasks
+open RunTask
 
 exception Ex
 
+let inline (^) x = x
+
 let verify n c = printfn "%s %s" (if c then "Ok" else "FAILURE") n
 
-let delayAndThen (ms: int) fn =
-  Task.toAlt <| fun ct ->
-  Task.Delay(ms, ct).ContinueWith(Func<_,_>(fn), ct)
+let delayAndSet (ms: int) r = Task.toAlt <| fun ct -> runTask {
+  do! Task.Delay (ms, ct)
+  return match Interlocked.CompareExchange (r, ms, 0) with
+          | 0 -> ms
+          | ms ->
+            printfn "Unexpected %d" ms
+            failwithf "Unexpected %d" ms
+}
 
-let delayAndSet r (ms: int) =
-  delayAndThen ms <| fun t ->
-  match Interlocked.CompareExchange(r, ms, 0) with
-   | 0 -> ms
-   | ms ->
-     printfn "Unexpected %d %A" ms t.IsCanceled
-     failwithf "Unexpected %d" ms
+let delayAndRaise (ms: int) ex = Task.toAlt <| fun ct -> runTask {
+  do! Task.Delay (ms, ct)
+  return raise ex
+}
 
 let run () =
   do let r = ref 0
-     (delayAndSet r 100 <|> delayAndSet r 50 |> run = 50 && !r = 50)
+     (delayAndSet 100 r <|> delayAndSet 50 r |> run = 50 && !r = 50)
      |> verify "Delays"
 
   do let r = ref 0
-     (delayAndSet r 50 <|> delayAndSet r 100 |> run = 50 && !r = 50)
+     (delayAndSet 50 r <|> delayAndSet 100 r |> run = 50 && !r = 50)
      |> verify "Delays"
 
-  do Job.tryIn (delayAndSet (ref 1) 50
-                <|> Task.toAlt (fun _ -> raise Ex ; Task.FromResult 1))
+  do Job.tryIn (delayAndSet 50 ^ ref 1
+            <|> Task.toAlt ^ fun _ -> raise Ex ; Task.FromResult 1)
          <| fun x -> printfn "Unexpected %A" x ; Job.unit ()
          <| fun e -> printfn "OK %A" e ; Job.unit ()
      |> run
 
-  do Job.tryIn (delayAndThen 50 (fun _ -> raise Ex)
-                <|> delayAndSet (ref 1) 100)
+  do Job.tryIn (delayAndRaise 50 Ex
+            <|> delayAndSet 100 ^ ref 1)
          <| fun x -> printfn "Unexpected %A" x ; Job.unit ()
          <| fun e -> printfn "OK %A" <| e.GetBaseException () ; Job.unit ()
      |> run
