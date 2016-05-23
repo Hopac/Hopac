eda7ab3ab70a3a97c99aae45a0cfb7f657e4a455
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-23 03:58:49 +0300

    Allowing ConfigureAwait.

--- Tests/AdHocTests/RunTask.fs
+++ Tests/AdHocTests/RunTask.fs
@@ -3,7 +3,12 @@
 module RunTask
 
 open System
+open System.Runtime.CompilerServices
 open System.Threading.Tasks
+
+type Stop () = class end
+let stop = Task.FromResult (Stop ()) :> Task
+let zero = Task.FromResult ()
 
 let inline (^) x = x
 let inline tryAp x2y x yK eK =
@@ -13,131 +18,114 @@ let inline tryAp x2y x yK eK =
    | null -> yK y
    | e -> eK e
 let inline tryIn u2x xK eK = tryAp u2x () xK eK
-let inline onCompleted xT2u (xT: #Task) =
-  xT.ConfigureAwait(false).GetAwaiter().UnsafeOnCompleted(fun () -> xT2u xT)
-let inline setTcs (tcs: TaskCompletionSource<_>) (xT: Task<_>) =
-  match xT.Status with
-   | TaskStatus.Canceled -> tcs.SetCanceled ()
-   | TaskStatus.Faulted  -> tcs.SetException xT.Exception
-   | _                   -> tcs.SetResult xT.Result
 let inline withTcs tcs2u =
   let tcs = TaskCompletionSource ()
   tcs2u tcs
   tcs.Task
-let stop = Task.FromResult () :> Task
-
-let inline result x = Task.FromResult x
-let zero = result ()
-let inline bind (x2yT: _ -> Task<_>) (xT: Task<_>) = withTcs <| fun tcs ->
-  xT
-  |> onCompleted ^ fun xT ->
-       tryIn <| fun () -> x2yT xT.Result
-        <| onCompleted ^ setTcs tcs
-        <| fun e ->
-             if xT.Status = TaskStatus.Canceled
-             then tcs.SetCanceled ()
-             else tcs.SetException e
-let inline bindTask (u2xT: _ -> Task<_>) (uT: Task) = withTcs <| fun tcs ->
-  uT
-  |> onCompleted ^ fun uT ->
-       match uT.Status with
-        | TaskStatus.Canceled -> tcs.SetCanceled ()
-        | TaskStatus.Faulted  -> tcs.SetException uT.Exception
-        | _ ->
-          tryIn u2xT
-           <| onCompleted ^ setTcs tcs
-           <| tcs.SetException
-let inline fromTask (uT: Task) =
-  match uT with
-   | :? Task<unit> as uT -> uT
-   | _ -> withTcs <| fun tcs ->
-     uT
-     |> onCompleted ^ fun uT ->
-          match uT.Status with
-           | TaskStatus.Canceled -> tcs.SetCanceled ()
-           | TaskStatus.Faulted  -> tcs.SetException uT.Exception
-           | _                   -> tcs.SetResult ()
-let inline whileDo (u2b: _ -> bool) (u2uT: _ -> #Task) = withTcs <| fun tcs ->
-  let rec loop () =
-    tryIn <| fun () -> if u2b () then u2uT () :> Task else stop
-     <| fun uT ->
-          if LanguagePrimitives.PhysicalEquality uT stop
-          then tcs.SetResult ()
-          else uT
-               |> onCompleted ^ fun uT ->
-                    match uT.Status with
-                     | TaskStatus.Canceled -> tcs.SetCanceled ()
-                     | TaskStatus.Faulted  -> tcs.SetException uT.Exception
-                     | _                   -> loop ()
-     <| tcs.SetException
-  loop ()
-let inline iterTask (x2uT: _ -> #Task) (xs: seq<_>) = withTcs <| fun tcs ->
-  tryIn xs.GetEnumerator
-   <| fun xs ->
-        let inline disposeAnd ef x = xs.Dispose () ; ef x
-        let rec loop () =
-          tryIn <| fun () -> if xs.MoveNext ()
-                             then x2uT xs.Current :> Task
-                             else stop
-           <| fun uT ->
-                if LanguagePrimitives.PhysicalEquality uT stop
-                then disposeAnd tcs.SetResult ()
-                else uT
-                     |> onCompleted ^ fun uT ->
-                          match uT.Status with
-                           | TaskStatus.Canceled ->
-                             disposeAnd tcs.SetCanceled ()
-                           | TaskStatus.Faulted ->
-                             uT.Exception |> disposeAnd tcs.SetException
-                           | _ ->
-                             loop ()
-           <| disposeAnd tcs.SetException
-        loop ()
-   <| tcs.SetException
-let inline tryFinally u2u (u2xT: _ -> Task<_>) = withTcs <| fun tcs ->
-  tryIn u2xT
-   <| onCompleted ^ fun xT ->
-        tryIn u2u
-         <| fun () -> setTcs tcs xT
-         <| tcs.SetException
-   <| tcs.SetException
-let inline tryWith (e2xT: _ -> Task<_>) (u2xT: _ -> Task<_>) = withTcs <| fun tcs ->
-  tryIn u2xT
-   <| onCompleted ^ fun xT ->
-        let s = xT.Status
-        if s = TaskStatus.RanToCompletion
-        then tcs.SetResult xT.Result
-        else tryAp e2xT <| if s = TaskStatus.Canceled
-                           then TaskCanceledException (xT) :> exn
-                           else xT.Exception :> exn
-              <| onCompleted ^ setTcs tcs
-              <| tcs.SetException
-   <| tcs.SetException
-let inline using (x2yT: _ -> Task<_>) (x: #IDisposable) = withTcs <| fun tcs ->
-  tryAp x2yT x
-   <| onCompleted ^ fun yT ->
-        tryIn x.Dispose
-         <| fun () -> setTcs tcs yT
-         <| tcs.SetException
+let inline setRes (tcs: TaskCompletionSource<_>) = tcs.SetResult
+let inline setExn (tcs: TaskCompletionSource<_>) (e: exn) =
+  match e with
+   | :? TaskCanceledException -> tcs.SetCanceled ()
+   | e -> tcs.SetException e
+let inline bindTo tcs (xT: Task<_>) =
+  let xA = xT.GetAwaiter ()
+  xA.OnCompleted(fun () ->
+    tryIn xA.GetResult
+     <| setRes tcs
+     <| setExn tcs)
+let inline bracket (w2xT: _ -> Task<_>) u2u w = withTcs <| fun tcs ->
+  tryAp w2xT w
+   <| fun xT ->
+        let xA = xT.GetAwaiter ()
+        xA.OnCompleted(fun () ->
+          tryIn (u2u >> xA.GetResult) (setRes tcs) (setExn tcs))
    <| fun e ->
-        tryIn x.Dispose
-         <| fun () -> tcs.SetException e
-         <| tcs.SetException
+        tryIn u2u
+         <| fun () -> setExn tcs e
+         <| setExn tcs
 
 type RunTaskBuilder () =
-  member __.Bind (uT, u2xT) = bindTask u2xT uT
-  member __.Bind (xT, x2yT) = bind x2yT xT
-  member __.Combine (uT, u2xT) = bindTask u2xT uT
-  member __.Delay (u2xT: unit -> Task<_>) = u2xT
-  member __.For (xs, x2uT) = iterTask x2uT xs
-  member __.Return x = result x
+  member __.Bind (wT: Task, w2xT: _ -> Task<_>) = withTcs <| fun tcs ->
+    let wA = wT.GetAwaiter()
+    wA.OnCompleted(fun () ->
+      tryIn (wA.GetResult >> w2xT) (bindTo tcs) (setExn tcs))
+  member __.Bind (wT: Task<_>, w2xT: _ -> Task<_>) = withTcs <| fun tcs ->
+    let wA = wT.GetAwaiter ()
+    wA.OnCompleted(fun () ->
+      tryIn (wA.GetResult >> w2xT) (bindTo tcs) (setExn tcs))
+  member __.Bind (wT: ConfiguredTaskAwaitable, w2xT: _ -> Task<_>) =
+    withTcs <| fun tcs ->
+    let wA = wT.GetAwaiter ()
+    wA.OnCompleted(fun () ->
+      tryIn (wA.GetResult >> w2xT) (bindTo tcs) (setExn tcs))
+  member __.Bind (wT: ConfiguredTaskAwaitable<_>, w2xT: _ -> Task<_>) =
+    withTcs <| fun tcs ->
+    let wA = wT.GetAwaiter ()
+    wA.OnCompleted(fun () ->
+      tryIn (wA.GetResult >> w2xT) (bindTo tcs) (setExn tcs))
+  member __.Combine (wT: Task, w2xT) = __.Bind (wT, w2xT)
+  member __.Delay (w2xT: unit -> Task<_>) = w2xT
+  member __.For (vs: seq<_>, v2wT: _ -> #Task) = withTcs <| fun tcs ->
+    tryIn vs.GetEnumerator
+     <| fun vs ->
+          let inline disposeAnd ef x =
+            try vs.Dispose () ; ef tcs x
+            with e -> setExn tcs e
+          let rec loop () =
+            tryIn <| fun () -> if vs.MoveNext ()
+                               then v2wT vs.Current :> Task
+                               else stop
+             <| fun wT ->
+                  if LanguagePrimitives.PhysicalEquality wT stop
+                  then disposeAnd setRes ()
+                  else let wA = wT.GetAwaiter ()
+                       wA.OnCompleted(fun () ->
+                         tryIn wA.GetResult loop (disposeAnd setExn))
+             <| disposeAnd setExn
+          loop ()
+     <| setExn tcs
+  member __.Return x = Task.FromResult x
+  member __.ReturnFrom (xT: Task) =
+    match xT with
+     | :? Task<unit> as xT -> xT
+     | _ -> withTcs <| fun tcs ->
+       let xA = xT.GetAwaiter ()
+       xA.OnCompleted(fun () ->
+         tryIn xA.GetResult (setRes tcs) (setExn tcs))
   member __.ReturnFrom (xT: Task<_>) = xT
-  member __.ReturnFrom uT = fromTask uT
-  member __.Run (u2xT: _ -> Task<_>) = u2xT ()
-  member __.TryFinally (u2xT, u2u) = tryFinally u2u u2xT
-  member __.TryWith (u2xT, e2xT) = tryWith e2xT u2xT
-  member __.Using (x, x2yT) = using x2yT x
-  member __.While (u2b, u2uT) = whileDo u2b u2uT
+  member __.ReturnFrom (xT: ConfiguredTaskAwaitable) = withTcs <| fun tcs ->
+    let xA = xT.GetAwaiter ()
+    xA.OnCompleted(fun () ->
+      tryIn xA.GetResult (setRes tcs) (setExn tcs))
+  member __.ReturnFrom (xT: ConfiguredTaskAwaitable<_>) = withTcs <| fun tcs ->
+    let xA = xT.GetAwaiter ()
+    xA.OnCompleted(fun () ->
+      tryIn xA.GetResult (setRes tcs) (setExn tcs))
+  member __.Run (w2xT: _ -> Task<_>) = w2xT ()
+  member __.TryFinally (w2xT: _ -> Task<_>, u2u) =
+    bracket w2xT u2u ()
+  member __.TryWith (w2xT: _ -> Task<_>, e2xT: _ -> Task<_>) =
+    withTcs <| fun tcs ->
+    let e2u e = tryAp e2xT e (bindTo tcs) (setExn tcs)
+    tryIn w2xT
+     <| fun xT ->
+          let xA = xT.GetAwaiter ()
+          xA.OnCompleted(fun () ->
+            tryIn xA.GetResult (setRes tcs) e2u)
+     <| e2u
+  member __.Using (x: #IDisposable, x2yT: _ -> Task<_>) =
+    bracket x2yT x.Dispose x
+  member __.While (u2b, u2wT: _ -> #Task) = withTcs <| fun tcs ->
+    let rec loop () =
+      tryIn <| fun () -> if u2b () then u2wT () :> Task else stop
+       <| fun wT ->
+            if LanguagePrimitives.PhysicalEquality wT stop
+            then setRes tcs ()
+            else let wA = wT.GetAwaiter ()
+                 wA.OnCompleted(fun () ->
+                   tryIn wA.GetResult loop (setExn tcs))
+       <| setExn tcs
+    loop ()
   member __.Zero () = zero
 
 let runTask = RunTaskBuilder ()
--- Tests/AdHocTests/RunTask.fsi
+++ Tests/AdHocTests/RunTask.fsi
@@ -3,22 +3,27 @@
 module RunTask
 
 open System
+open System.Runtime.CompilerServices
 open System.Threading.Tasks
 
 type RunTaskBuilder =
   new: unit -> RunTaskBuilder
-  member Bind: Task * (unit -> Task<'x>) -> Task<'x>
-  member Bind: Task<'x> * ('x -> Task<'y>) -> Task<'y>
+  member Bind:                    Task     * (unit -> Task<'x>) -> Task<'x>
+  member Bind:                    Task<'w> * ('w   -> Task<'x>) -> Task<'x>
+  member Bind: ConfiguredTaskAwaitable     * (unit -> Task<'x>) -> Task<'x>
+  member Bind: ConfiguredTaskAwaitable<'w> * ('w   -> Task<'x>) -> Task<'x>
   member Combine: Task * (unit -> Task<'x>) -> Task<'x>
   member Delay: (unit -> Task<'x>) -> (unit -> Task<'x>)
   member For: seq<'x> * ('x -> #Task) -> Task<unit>
   member Return: 'x -> Task<'x>
-  member ReturnFrom: Task<'x> -> Task<'x>
-  member ReturnFrom: Task -> Task<unit>
+  member ReturnFrom:                    Task     -> Task<unit>
+  member ReturnFrom:                    Task<'x> -> Task<'x>
+  member ReturnFrom: ConfiguredTaskAwaitable     -> Task<unit>
+  member ReturnFrom: ConfiguredTaskAwaitable<'x> -> Task<'x>
   member Run: (unit -> Task<'x>) -> Task<'x>
   member TryFinally: (unit -> Task<'x>) * (unit -> unit) -> Task<'x>
   member TryWith: (unit -> Task<'x>) * (exn -> Task<'x>) -> Task<'x>
-  member Using: 'x * ('x -> Task<'y>) -> Task<'y> when 'x :> IDisposable
+  member Using: 'w * ('w -> Task<'x>) -> Task<'x> when 'w :> IDisposable
   member While: (unit -> bool) * (unit -> #Task) -> Task<unit>
   member Zero: unit -> Task<unit>
 
