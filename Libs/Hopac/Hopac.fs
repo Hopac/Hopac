7d07610c2a22a7c4b52227be6fabb2815d5b3b59
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-05-29 01:06:26 +0300

    Use the standard `member __.` idiom.

--- Libs/Hopac/Hopac.fs
+++ Libs/Hopac/Hopac.fs
@@ -2185,36 +2185,36 @@ open Extensions
 ////////////////////////////////////////////////////////////////////////////////
 
 type JobBuilder () =
-  member inline job.Bind (xO: IObservable<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
+  member inline __.Bind (xO: IObservable<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
     xO.onceAlt >>= x2yJ
-  member inline job.Bind (xA: Async<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
+  member inline __.Bind (xA: Async<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
     Async.toJob xA >>= x2yJ
-  member inline job.Bind (xT: Task<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
+  member inline __.Bind (xT: Task<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
     Task.bindJob (xT, x2yJ)
-  member inline job.Bind (uT: Task, u2xJ: unit -> Job<'x>) : Job<'x> =
+  member inline __.Bind (uT: Task, u2xJ: unit -> Job<'x>) : Job<'x> =
     Task.bindJob (uT, u2xJ)
-  member inline job.Bind (xJ: Job<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
+  member inline __.Bind (xJ: Job<'x>, x2yJ: 'x -> Job<'y>) : Job<'y> =
     xJ >>= x2yJ
-  member inline job.Combine (uJ: Job<unit>, xJ: Job<'x>) : Job<'x> = uJ >>=. xJ
-  member inline job.Delay (u2xJ: unit -> Job<'x>) : Job<'x> = Job.delay u2xJ
-  member inline job.For (xs: seq<'x>, x2uJ: 'x -> Job<unit>) : Job<unit> =
+  member inline __.Combine (uJ: Job<unit>, xJ: Job<'x>) : Job<'x> = uJ >>=. xJ
+  member inline __.Delay (u2xJ: unit -> Job<'x>) : Job<'x> = Job.delay u2xJ
+  member inline __.For (xs: seq<'x>, x2uJ: 'x -> Job<unit>) : Job<unit> =
     Seq.iterJob x2uJ xs
-  member inline job.Return (x: 'x) : Job<'x> = Job.result x
-  member inline job.ReturnFrom (xO: IObservable<'x>) = xO.onceAlt :> Job<_>
-  member inline job.ReturnFrom (xA: Async<'x>) : Job<'x> = Async.toJob xA
-  member inline job.ReturnFrom (xT: Task<'x>) : Job<'x> = Task.awaitJob xT
-  member inline job.ReturnFrom (uT: Task) : Job<unit> = Task.awaitJob uT
-  member inline job.ReturnFrom (xJ: Job<'x>) : Job<'x> = xJ
-  member inline job.TryFinally (xJ: Job<'x>, u2u: unit -> unit) : Job<'x> =
+  member inline __.Return (x: 'x) : Job<'x> = Job.result x
+  member inline __.ReturnFrom (xO: IObservable<'x>) = xO.onceAlt :> Job<_>
+  member inline __.ReturnFrom (xA: Async<'x>) : Job<'x> = Async.toJob xA
+  member inline __.ReturnFrom (xT: Task<'x>) : Job<'x> = Task.awaitJob xT
+  member inline __.ReturnFrom (uT: Task) : Job<unit> = Task.awaitJob uT
+  member inline __.ReturnFrom (xJ: Job<'x>) : Job<'x> = xJ
+  member inline __.TryFinally (xJ: Job<'x>, u2u: unit -> unit) : Job<'x> =
     Job.tryFinallyFun xJ u2u
-  member inline job.TryWith (xJ: Job<'x>, e2xJ: exn -> Job<'x>) : Job<'x> =
+  member inline __.TryWith (xJ: Job<'x>, e2xJ: exn -> Job<'x>) : Job<'x> =
     Job.tryWith xJ e2xJ
-  member inline job.Using (x: 'x, x2yJ: 'x -> Job<'y>) : Job<'y>
+  member inline __.Using (x: 'x, x2yJ: 'x -> Job<'y>) : Job<'y>
       when 'x :> IDisposable =
     Job.using x x2yJ
-  member inline job.While (u2b: unit -> bool, uJ: Job<unit>) : Job<unit> =
+  member inline __.While (u2b: unit -> bool, uJ: Job<unit>) : Job<unit> =
     Job.whileDo u2b uJ
-  member inline job.Zero () : Job<unit> = Job.unit ()
+  member inline __.Zero () : Job<unit> = Job.unit ()
 
 type EmbeddedJob<'x> = struct
     val Job: Job<'x>
