88a42523d83bd62b730f8d97bdc384ee67396474
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-06-01 04:37:31 +0300

    Check both exceptions are reported.

--- Tests/AdHocTests/JobTests.fs
+++ Tests/AdHocTests/JobTests.fs
@@ -17,3 +17,8 @@ let run () =
   let dummy = new Dummy (1)
   Job.using dummy Job.result |> run |> testEq dummy
   Job.using (null : IDisposable) Job.result |> run |> testEq null
+
+  Job.delay (fun () -> raise ^ Expected 1) <*>
+  Job.delay (fun () -> raise ^ Expected 2)
+  |> Job.catch |> run
+  |> testExpected [Expected 1; Expected 2]
