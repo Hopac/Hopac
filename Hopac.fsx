f99f0266c7f61bec8e0a46f0e1f5b8d5106680f1
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-07-13 22:38:20 +0300

    No need for semis.

--- Hopac.fsx
+++ Hopac.fsx
@@ -1,18 +1,18 @@
-#I "Libs/Hopac.Core/bin/Release" ;;
-#I "Libs/Hopac/bin/Release" ;;
-#I "Libs/Hopac.Extra/bin/Release" ;;
-#I "Libs/Hopac.Experimental/bin/Release" ;;
-#I "Libs/Hopac.Platform.Net/bin/Release" ;;
+#I "Libs/Hopac.Core/bin/Release"
+#I "Libs/Hopac/bin/Release"
+#I "Libs/Hopac.Extra/bin/Release"
+#I "Libs/Hopac.Experimental/bin/Release"
+#I "Libs/Hopac.Platform.Net/bin/Release"
 
-#r "Hopac.Core.dll" ;;
-#r "Hopac.dll" ;;
-#r "Hopac.Extra.dll" ;;
-#r "Hopac.Experimental.dll" ;;
-#r "Hopac.Platform.dll" ;;
+#r "Hopac.Core.dll"
+#r "Hopac.dll"
+#r "Hopac.Extra.dll"
+#r "Hopac.Experimental.dll"
+#r "Hopac.Platform.dll"
 
-open System ;;
-open Hopac.Experimental ;;
-open Hopac.Extensions ;;
-open Hopac.Extra ;;
-open Hopac.Infixes ;;
-open Hopac ;;
+open System
+open Hopac.Experimental
+open Hopac.Extensions
+open Hopac.Extra
+open Hopac.Infixes
+open Hopac
