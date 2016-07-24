04d83729fabf21a54c2c1061a0dc9c119625462c
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-07-24 22:37:42 +0300

    Trying again with strong name

--- Libs/Hopac.Core/Assembly.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) by Housemarque, Inc.
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// Hopac.Core is really only intended to be used by other Hopac libraries.
-[assembly: InternalsVisibleTo("Hopac")]
-[assembly: InternalsVisibleTo("Hopac.Platform")]
--- Libs/Hopac.Core/AssemblyInfo.cs
+++ Libs/Hopac.Core/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 // NOTE: This is a generated file.
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleToAttribute("Hopac")]
+[assembly: InternalsVisibleToAttribute("Hopac.Platform")]
 [assembly: AssemblyTitleAttribute("Hopac.Core")]
 [assembly: AssemblyProductAttribute("Hopac.Core")]
 [assembly: AssemblyDescriptionAttribute("A library for Higher-Order, Parallel, Asynchronous and Concurrent programming in F#.")]
