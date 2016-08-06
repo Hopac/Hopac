a765e1fc6116805e992081458aac720a08a835d2
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-08-06 11:54:43 +0300

    0.3.5

--- Libs/Hopac.Platform.Net/AssemblyInfo.fs
+++ Libs/Hopac.Platform.Net/AssemblyInfo.fs
@@ -7,8 +7,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Hopac.Platform.Net")>]
 [<assembly: AssemblyProductAttribute("Hopac.Platform.Net")>]
 [<assembly: AssemblyDescriptionAttribute("A library for Higher-Order, Parallel, Asynchronous and Concurrent programming in F#.")>]
-[<assembly: AssemblyVersionAttribute("0.3.4")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.4")>]
+[<assembly: AssemblyVersionAttribute("0.3.5")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.5")>]
 [<assembly: AssemblyCompanyAttribute("Housemarque Inc.")>]
 [<assembly: AssemblyCopyrightAttribute("© Housemarque Inc.")>]
 
