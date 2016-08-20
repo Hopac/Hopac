bd3bf7b47b71c4c621fdc7e87adbf62e16d11269
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-08-20 14:13:51 +0300

    Hopac.Extra removed

--- Hopac.fsx
+++ Hopac.fsx
@@ -1,18 +1,15 @@
 #I "Libs/Hopac.Core/bin/Release"
 #I "Libs/Hopac/bin/Release"
-#I "Libs/Hopac.Extra/bin/Release"
 #I "Libs/Hopac.Experimental/bin/Release"
 #I "Libs/Hopac.Platform.Net/bin/Release"
 
 #r "Hopac.Core.dll"
 #r "Hopac.dll"
-#r "Hopac.Extra.dll"
 #r "Hopac.Experimental.dll"
 #r "Hopac.Platform.dll"
 
 open System
 open Hopac.Experimental
 open Hopac.Extensions
-open Hopac.Extra
 open Hopac.Infixes
 open Hopac
