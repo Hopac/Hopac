1d377f12d56dd87b7296cbfd8a590ea5d3e4791d
Henrik <henrik@haf.se>
2018-09-19 14:48:57 +0200

    Correcting #r lines in Hopac.fsx

--- Hopac.fsx
+++ Hopac.fsx
@@ -1,14 +1,14 @@
-#I "Libs/Hopac.Core/bin/Release"
-#I "Libs/Hopac/bin/Release"
-#I "Libs/Hopac.Platform/bin/Release"
-#I "Libs/Hopac.Bench/bin/Release"
-#I "Libs/Hopac.Experimental/bin/Release"
+#I "Libs/Hopac.Core/bin/Release/net471"
+#I "Libs/Hopac/bin/Release/net471"
+#I "Libs/Hopac.Platform/bin/Release/net471"
+#I "Libs/Hopac.Bench/bin/Release/net471"
+#I "Libs/Hopac.Experimental/bin/Release/net471"
 
-#r "Hopac.Core.dll"
-#r "Hopac.dll"
-#r "Hopac.Platform.dll"
-#r "Hopac.Bench.dll"
-#r "Hopac.Experimental.dll"
+#r "Hopac.Core"
+#r "Hopac"
+#r "Hopac.Platform"
+#r "Hopac.Bench"
+#r "Hopac.Experimental"
 
 open System
 open System.Threading
@@ -17,3 +17,5 @@ open Hopac.Experimental
 open Hopac.Extensions
 open Hopac.Infixes
 open Hopac
+
+// Write your code here
