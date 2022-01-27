6bd44c8efb6c53a0066354841aded246dbb4c88d
Charlie Turndorf <dev@wizardgalaxy.com>
2022-01-27 16:51:45 -0500

    Bootstrap Hopac.Tests
    - Added Tests/Hopac.Tests project using Expecto.FsCheck
    - Build & run is working but no tests are in yet
    - Added test script for both cmd and bash

---
+++ Tests/Hopac.Tests/Util.fs
@@ -0,0 +1,21 @@
+[<AutoOpen>]
+module Hopac.Tests.Util
+
+open System.Reflection
+open FSharp.Reflection
+
+
+/// Defines cleanup logic that should be run after testing
+type CleanupAttribute() = inherit System.Attribute()
+
+/// Runs all module-scope functions (without any parameters)
+/// that are marked with the Cleanup attribute
+let runCleanupInAssembly () =
+    Assembly.GetEntryAssembly().GetTypes()
+    |> Seq.filter FSharpType.IsModule
+    |> Seq.collect (fun t -> t.GetMethods())
+    |> Seq.filter (fun m ->
+        m.CustomAttributes |> Seq.exists (fun a ->
+            a.AttributeType = typeof<CleanupAttribute>)
+        && m.GetParameters().Length = 0)
+    |> Seq.iter (fun m -> m.Invoke(null, [||]) |> ignore)
